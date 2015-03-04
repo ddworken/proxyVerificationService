617b374c32d56d2839169f17c22ad4a52e20f873
David Dworken <david@daviddworken.com>
2015-03-04 17:24:15 -0500

    Added more useful debugging output

--- proxyVerifyAPI.py
+++ proxyVerifyAPI.py
@@ -27,17 +27,17 @@ def verifyProxy(proxyIP):
         try:
             html = requests.get("http://www.daviddworken.com/", timeout=10, proxies = {'http':'http://'+proxyIP}, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.69 Safari/537.36'})
         except:
-            print proxyIP + "0"
+            print proxyIP + " Status: 0"
             return {"Status:" : 0, "Reason": "Proxy not responding to requests"}
     else:
-        print proxyIP + "0"
+        print proxyIP + " Status: 0"
         return {"Status:" : 0, "Reason": "Proxy not responding to requests"}
 
     try:
         proxyHTML = requests.get("http://www.daviddworken.com/", proxies = {'http':'http://'+proxyIP}, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.69 Safari/537.36'})
         normalHTML = requests.get(proxyIP, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.69 Safari/537.36'})
         if normalHTML.content == proxyHTML.content:
-            print proxyIP + "4"
+            print proxyIP + " Status: 4"
             return {"Status:" : 4, "Reason": "The proxy is not a proxy, it is just a web server"}
         print normalHTML.content
         print proxyHTML.content
@@ -57,21 +57,21 @@ def verifyProxy(proxyIP):
             diff = differ.compare(htmlNormalL, htmlL)
             #print(bcolors.WARNING + "[-] Malicious proxy found at " + proxy + bcolors.ENDC)
             #diffOut =  '\n'.join(diff)
-            print proxyIP + "1"
+            print proxyIP + " Status: 1"
             return {"Status:" : 1, "Reason": "Proxy modifies HTML"}
     try:
         html = requests.get("https://example.com", proxies = {'http':'http://'+proxyIP}, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.69 Safari/537.36'})
     except:
-        print proxyIP + "2"
+        print proxyIP + " Status: 2"
         return {"Status:" : 2, "Reason": "Proxy fails to connect over SSL (MITM attack is likely)"}
     try:
         html = requests.get("http://whatismyipaddress.com/proxy-check", proxies = {'http':'http://'+proxyIP}, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.69 Safari/537.36'})
         if len([m.start() for m in re.finditer('FALSE', html.content)]) != 6 and 'TRUE' in html.content:
-            print proxyIP + "3"
+            print proxyIP + " Status: 3"
             return {"Status:" : 3, "Reason": "Proxy is not fully anonymous. "}
     except:
         pass
-    print proxyIP + "-1"
+    print proxyIP + " Status: -1"
     return {"Status:" : -1, "Reason": "Proxy passed all tests"}
 
 if __name__ == '__main__':
